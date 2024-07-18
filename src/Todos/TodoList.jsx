4b7bfd7094d25a097f82a8f4205f8da066c401da
Nika Donchenko <donchenko.veronika96@gmail.com>
2024-07-18 09:06:47 +0200

    double-click to remove a task from the list

--- src/Todos/TodoList.jsx
+++ src/Todos/TodoList.jsx
@@ -1,11 +1,11 @@
 import Todo from './Todo';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, deleteTodo }) => {
   return (
     <div className="">
       {!todos.length && <h2>Todo list is empty</h2>}
       {todos.map((todo, index) => (
-        <Todo key={index} todo={todo} />
+        <Todo key={index} todo={todo} index={index} deleteTodo={deleteTodo} />
       ))}
     </div>
   );
